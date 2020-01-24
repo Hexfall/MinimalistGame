45e06e305144f9111d0b897730529f353b09bd4f
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2020-01-24 14:49:30 +0000

    removed unnecessary code.

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -8,14 +8,6 @@ public class AsteroidSpawner : MonoBehaviour
 	public float spawnTime;
 	private float lastSpawn = 0.0f;
 	public GameObject asteroid;
-	private Vector2 screenBounds;
-	private float hypo;
-
-	void Start()
-	{
-		screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-		hypo = 2 * screenBounds.y;
-	}
 
     // Update is called once per frame
     void FixedUpdate()
